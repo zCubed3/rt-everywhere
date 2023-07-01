97049991912f4a6e59b15095d38b2810848ed39b
zCubed3 <zcubedtres@gmail.com>
2023-07-01 08:23:59 -0700

    MSAA

--- core/rt_everywhere.c
+++ core/rt_everywhere.c
@@ -24,6 +24,14 @@
 
 #include <math.h>
 
+#define RT_EVERYWHERE_MSAA
+
+#ifdef RT_EVERYWHERE_MSAA
+#define RT_EVERYWHERE_SAMPLES 4
+#else
+#define RT_EVERYWHERE_SAMPLES 1
+#endif
+
 void screen_to_viewport(rvec2_t dst, viewport_t viewport, point_t point) {
 	// Note: When x == 0, x / width = 0, but x never hits width
 	// Therefore we must add half the texel size to x to account for this
@@ -84,6 +92,8 @@ int trace_scene(fragment_t *p_fragment, ray_t ray) {
 	p_fragment->material_type = MATERIAL_TYPE_OPAQUE;
 
 	// Intersect the ground
+	const real_t GROUND_CHECKER_SIZE = REAL(2.0);
+
 	real_t closest_t = 100;
 	real_t ground_t = -ray.origin[1] / ray.direction[1];
 	if (ground_t > 0 && ground_t < closest_t) {
@@ -99,8 +109,8 @@ int trace_scene(fragment_t *p_fragment, ray_t ray) {
 		rvec3_t checker;
 		rvec3_copy(checker, p_fragment->position);
 
-		checker[0] = real_floor(checker[0]);
-		checker[2] = real_floor(checker[2]);
+		checker[0] = real_floor(checker[0] * GROUND_CHECKER_SIZE);
+		checker[2] = real_floor(checker[2] * GROUND_CHECKER_SIZE);
 
 		real_t mod = real_mod(checker[0] + real_mod(checker[2], REAL(2.0)), REAL(2.0));
 
@@ -163,69 +173,97 @@ void shade_fragment(rvec3_t dst_col, fragment_t fragment, ray_t ray) {
 }
 
 void trace_pixel(rvec3_t dst_col, camera_t camera, point_t point) {
-	// Clear color
-	rvec3_copy(dst_col, (rvec3_t) {REAL(0.1), REAL(0.1), REAL(0.1)});
+	rvec3_copy(dst_col, (rvec3_t) {0, 0, 0});
 
-	// Set up the base ray
-	rvec2_t view_coord;
-	screen_to_viewport(view_coord, camera.viewport, point);
-
-	ray_t ray;
-
-	rvec3_copy(ray.direction, (rvec3_t){view_coord[0], -view_coord[1], 1});
-
-#ifdef RTEVERYWHERE_FLIP_Y
-	ray.direction[1] *= -1;
+#ifdef RT_EVERYWHERE_MSAA
+	real_t sub_tex_x = REAL(1.0) / (real_t)camera.viewport.width;
+	real_t sub_tex_y = REAL(1.0) / (real_t)camera.viewport.height;
 #endif
 
-	rvec4_t pre_t;
-	rvec4_t post_t;
+	// Clear color
+	for (int s = 0; s < RT_EVERYWHERE_SAMPLES; s++) {
+		rvec3_t sample;
+		rvec3_copy(sample, (rvec3_t) {REAL(0.1), REAL(0.1), REAL(0.1)});
 
-	rvec4_copy_rvec3_w(pre_t, ray.direction, REAL(1.0));
-	rmat4_mul_rvec4(post_t, camera.mat_vp_i, pre_t);
+		// Set up the base ray
+		// It's jittered at a subpixel level when using MSAA
+		rvec2_t view_coord;
+		screen_to_viewport(view_coord, camera.viewport, point);
 
-	rvec3_copy_rvec4(ray.direction, post_t);
-	rvec3_normalize(ray.direction);
+#ifdef RT_EVERYWHERE_MSAA
+		if (s <= 1) {
+			view_coord[0] += sub_tex_x * REAL(0.5);
+		} else {
+			view_coord[0] -= sub_tex_x * REAL(0.5);
+		}
 
-	rvec4_copy_rvec3_w(pre_t, (rvec3_t){0, 0, 0}, REAL(1.0));
-	rmat4_mul_rvec4(post_t, camera.mat_v, pre_t);
+		if (s % 2 == 0) {
+			view_coord[1] += sub_tex_y * REAL(0.5);
+		} else {
+			view_coord[1] -= sub_tex_y * REAL(0.5);
+		}
+#endif
 
-	rvec3_copy_rvec4(ray.origin, post_t);
+		ray_t ray;
 
-	//
-	// Base pass
-	//
-	fragment_t base_frag;
-	if (trace_scene(&base_frag, ray)) {
-		shade_fragment(dst_col, base_frag, ray);
+		rvec3_copy(ray.direction, (rvec3_t) {view_coord[0], -view_coord[1], 1});
 
-		// Reflection
-		if (base_frag.material_type == MATERIAL_TYPE_MIRROR) {
-			rvec3_t bias;
-			rvec3_copy(bias, base_frag.normal);
-			rvec3_mul_scalar(bias, bias, REAL(0.00001));
+#ifdef RTEVERYWHERE_FLIP_Y
+		ray.direction[1] *= -1;
+#endif
 
-			fragment_t reflect_frag;
-			ray_t reflect_ray;
+		rvec4_t pre_t;
+		rvec4_t post_t;
 
-			rvec3_copy(reflect_ray.origin, base_frag.position);
-			rvec3_add(reflect_ray.origin, reflect_ray.origin, bias);
+		rvec4_copy_rvec3_w(pre_t, ray.direction, REAL(1.0));
+		rmat4_mul_rvec4(post_t, camera.mat_vp_i, pre_t);
 
-			rvec3_t view_dir;
-			rvec3_copy(view_dir, ray.direction);
+		rvec3_copy_rvec4(ray.direction, post_t);
+		rvec3_normalize(ray.direction);
 
-			rvec3_t incidence;
-			rvec3_reflect(incidence, view_dir, base_frag.normal);
-			rvec3_normalize(incidence);
+		rvec4_copy_rvec3_w(pre_t, (rvec3_t) {0, 0, 0}, REAL(1.0));
+		rmat4_mul_rvec4(post_t, camera.mat_v, pre_t);
 
-			rvec3_copy(reflect_ray.direction, incidence);
+		rvec3_copy_rvec4(ray.origin, post_t);
 
-			if (trace_scene(&reflect_frag, reflect_ray)) {
-				shade_fragment(dst_col, reflect_frag, reflect_ray);
-			} else {
-				rvec3_copy(dst_col, (rvec3_t) {REAL(0.1), REAL(0.1), REAL(0.1)});
+		//
+		// Base pass
+		//
+		fragment_t base_frag;
+		if (trace_scene(&base_frag, ray)) {
+			shade_fragment(sample, base_frag, ray);
+
+			// Reflection
+			if (base_frag.material_type == MATERIAL_TYPE_MIRROR) {
+				rvec3_t bias;
+				rvec3_copy(bias, base_frag.normal);
+				rvec3_mul_scalar(bias, bias, REAL(0.00001));
+
+				fragment_t reflect_frag;
+				ray_t reflect_ray;
+
+				rvec3_copy(reflect_ray.origin, base_frag.position);
+				rvec3_add(reflect_ray.origin, reflect_ray.origin, bias);
+
+				rvec3_t view_dir;
+				rvec3_copy(view_dir, ray.direction);
+
+				rvec3_t incidence;
+				rvec3_reflect(incidence, view_dir, base_frag.normal);
+				rvec3_normalize(incidence);
+
+				rvec3_copy(reflect_ray.direction, incidence);
+
+				if (trace_scene(&reflect_frag, reflect_ray)) {
+					shade_fragment(sample, reflect_frag, reflect_ray);
+				} else {
+					rvec3_copy(sample, (rvec3_t) {REAL(0.1), REAL(0.1), REAL(0.1)});
+				}
 			}
 		}
+
+		rvec3_mul_scalar(sample, sample, REAL(1.0) / (real_t)RT_EVERYWHERE_SAMPLES);
+		rvec3_add(dst_col, dst_col, sample);
 	}
 
 	//
